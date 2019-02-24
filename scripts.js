1d182a2447c59d91918492a7b4b7b7dc3178bf79
Salvi Pascual <salvi.pascual@gmail.com>
2019-02-23 20:45:41 -0500

    fix issues

--- scripts.js
+++ scripts.js
@@ -1,52 +1,56 @@
 $(document).ready(function () {
-    $('.modal').modal();
+	$('.modal').modal();
 });
 
 function sendMessage() {
-    let message = $('#message').val().trim();
-    if(message.length>=30){
-        apretaste.send({
-            'command':'SOPORTE ESCRIBIR',
-            'data':{'message':message},
-            'redirect':false,
-            'callback':{'name':'sendMessageCallback','data':message}
-        });
-    }
-    else showToast('Minimo 30 caracteres');
+	let message = $('#message').val().trim();
+	if(message.length>=30){
+		apretaste.send({
+			'command':'SOPORTE ESCRIBIR',
+			'data':{'message':message},
+			'redirect':false,
+			'callback':{'name':'sendMessageCallback','data':message}
+		});
+	}
+	else M.toast({html: 'Mínimo 30 caracteres'});
 }
 
 function messageLengthValidate() {
-    let message = $('#message').val().trim();
-    if(message.length<=1000) $('.helper-text').html('Restante: '+(1000-message.length));
-    else $('.helper-text').html('Limite excedido');
+	let message = $('#message').val().trim();
+	if(message.length<=1000) $('.helper-text').html('Restante: '+(1000-message.length));
+	else $('.helper-text').html('Limite excedido');
 }
 
-function sendMessageCallback(message){
-    if($('#main .chat').length==0){
-        $('#main p').remove();
-        $('#main').append('<h5 class="center-align">Su conversación con el soporte</h5>')
-        $('#main').append('<div class="chat"></div>')
-    };
-    let now = new Date(Date.now()).toLocaleString();
-    now = now.replace('p. m.','pm');
-    now = now.replace('a. m.','am');
-    
-    $('.chat').append(`
-            <div class="bubble me">
-                <small>
-                    <b>`+myUsername+`</b> - `+now+` 
-                    <div class="chip small white-text blue lighten-1">NEW</div>
-                </small>
-                <br>
-                `+message+`
-            </div>
-    `);
+function sendMessageCallback(message) {
+	// if first time, change headers
+	if($('#main .chat').length == 0) {
+		$('#main h1').remove();
+		$('#main p').remove();
+		$('#main').append('<h1>Su conversación con el soporte</h1>');
+		$('#main').append('<div class="chat"></div>');
+	};
 
-    $('html, body').animate({
-        scrollTop: $(".bubble:last-of-type").offset().top
-    }, 1000);
-}
+	// create bubble date
+	let now = new Date(Date.now()).toLocaleString();
+	now = now.replace('p. m.','pm');
+	now = now.replace('a. m.','am');
 
-function showToast(text){
-    M.toast({html: text});
+	// append the bubble to teh screen
+	$('.chat').append(`
+		<div class="bubble me">
+			<small>
+				<b>`+myUsername+`</b> - `+now+` 
+				<div class="chip small white-text blue lighten-1">NEW</div>
+			</small>
+			<br>
+			`+message+`
+		</div>`);
+
+	// scroll to the last bubble
+	var lastBubble = $(".bubble:last-of-type");
+	if(lastBubble) {
+		$('html, body').animate({
+			scrollTop: $(".bubble:last-of-type").offset().top
+		}, 1000);
+	}
 }
--- service.php
+++ service.php
@@ -3,54 +3,58 @@
 class Service
 {
 	/**
-	 * Function executed when the service is called
+	 * Show the list of messages
 	 *
 	 * @param Request
 	 * @param Response
 	 */
-	public function _main(Request $request, Response $response){
+	public function _main(Request $request, Response $response)
+	{
 		// @TODO replace email with ids
 		$userEmail = Utils::getPerson($request->person->id)->email;
 		$username = $request->person->username;
 
 		// get the list of messages
-		$tickets = Connection::query(
-			"SELECT *
+		$tickets = Connection::query("
+			SELECT *
 			FROM support_tickets
 			WHERE `from` = '$userEmail'
 			OR requester = '$userEmail'
 			ORDER BY creation_date ASC");
-		
+
+		// prepare chats for the view
 		$chat = [];
 		$usernames = [$userEmail => $username];
-		foreach($tickets as $ticket){
+		foreach($tickets as $ticket) {
 			$from = $ticket->from;
 			$requester = $ticket->requester;
 
 			// dont lookup for the same email twice
-			if(!in_array($from, $usernames)) $usernames[$from] = Utils::getPerson($from)->username;
+			if( ! in_array($from, $usernames)) $usernames[$from] = Utils::getPerson($from)->username;
 
 			$message = new stdClass();
-			$message->from = $usernames[$from]; //replace emails with usernames, don't send emails in the data
+			$message->from = $usernames[$from]; //replace emails with usernames
 			$message->text = $ticket->body;
 			$message->date = date_format((new DateTime($ticket->creation_date)),'d/m/Y h:i a');
 			$message->status = $ticket->status;
 			$message->class = $from==$userEmail ? "me":"you";
 			$chat[] = $message;
 		}
-		
-		$response->setTemplate('main.ejs',['chat' => $chat, 'myUsername' => $username]);
+
+		// send data to the view
+		$response->setTemplate('home.ejs',['chat' => $chat, 'myUsername' => $username]);
 	}
 
 	/**
 	 * Create a new ticket
+	 *
 	 * @param Request $request
 	 * @param Response $response
 	 */
-	public function _escribir(Request $request, Response $response){
+	public function _escribir(Request $request, Response $response)
+	{
 		$message = $request->input->data->message;
-		// @TODO replace email with ids
-		$userEmail = Utils::getPerson($request->person->id)->email;
+		$userEmail = Utils::getPerson($request->person->id)->email; // @TODO replace email with ids
 
 		// insert ticket
 		$body = Connection::escape($message, 1024);
